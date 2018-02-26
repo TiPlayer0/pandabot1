33635bafcb7d4bc790370af4504b0dbf9d2bd7b4
TiPlayer0 <36779146+TiPlayer0@users.noreply.github.com>
2018-02-26 18:35:08 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-var prefix = ("*")
+var prefix = (";")
 
 bot.on('ready', function() {
     bot.user.setActivity("Work-in progres");
@@ -12,21 +12,17 @@ bot.login("NDE2NzExOTA0MzI5ODU5MDcy.DXTVCQ.dMo7DbMxfD2grXGqTl8IssIxXow");
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.send("Liste des commandes: \n -*help");
-    }})
-    
-bot.on('message', message => {
+        message.channel.send("Liste des commandes: \n -help");
+    }
     if (message.content === prefix + "tt"){
         message.channel.send("Le Bot a était crée le 26/02/2018 à 01:04");
-    }})
-    
-bot.on('message', message => {
+    }
+
     if (message.content === prefix + "fabriquand"){
         message.channel.send("Le Bot a était crée par TiPlayer_0");
-    }})
+    }
 
     if (message.content === "Salut"){
         message.reply("Bien le bonjour. =D");
         console.log("Commande Salut effectué");
- 
-    }
+}})

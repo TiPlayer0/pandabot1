77158b36e7baa888e9e5807dacb9cf7282e1f840
TiPlayer0 <36779146+TiPlayer0@users.noreply.github.com>
2018-03-30 13:38:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,55 +1,14 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-var prefix = (";")
+client.on("ready", () => {
+  console.log("I am ready!");
+});
 
-bot.user.setPresence({ status: 'online', game: { name: 'GAME' } });
-
-bot.on('ready', function () {
-  console.log("Je suis connecté !")
-})
-
-bot.login('TOKEN')
-
-bot.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong !')
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
   }
-})
+});
 
-bot.on('guildMemberAdd', member => {
-  member.createDM().then(channel => {
-    return channel.send('Bienvenue sur mon serveur ' + member.displayName)
-  }).catch(console.error)
-})
-
-bot.on('message', message => {
-
-  if (message.content.startsWith('!play')) {
-    let voiceChannel = message.guild.channels
-      .filter(function (channel) { return channel.type === 'voice' })
-      .first()
-    let args = message.content.split(' ')
-    voiceChannel
-      .join()
-      .then(function (connection) {
-        let stream = YoutubeStream(args[1])
-        stream.on('error', function () {
-          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-          connection.disconnect()
-        })
-        connection
-          .playStream(stream)
-          .on('end', function () {
-            connection.disconnect()
-          })
-      })
-  }
-
-})
-
-bot.on('message', message => {
-  if (message.content === 'Salut a tous') {
-    message.reply('Bien le bonjour a toi !')
-  }
-})
+client.login("TOKEN");

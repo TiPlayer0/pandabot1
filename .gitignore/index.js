d413fbcc0029c69fe479168049b9d30b33f55ca9
TiPlayer0 <36779146+TiPlayer0@users.noreply.github.com>
2018-03-30 13:41:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("ready", () => {
-  console.log("I am ready!");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
   }
 });
 
-client.login("TOKEN");
+client.login('TOKEN');
